30834728f7496ab713fa3a73e9a2d4794b6f4f4c
Siddami <dharmmy621@gmail.com>
2023-05-16 17:42:01 +0100

    day 6 of challenge

---
+++ days of js/day6_js/level1.js
@@ -0,0 +1,65 @@
+//1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+//for loop
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// //while
+// let i = 0;
+// while(i <= 10){
+//     console.log(i)
+//     i++;
+// }
+
+// //do while
+// let index = 0;
+// do {
+//     console.log(index);
+//     index++
+// }while (index <= 10);
+
+// //2. Iterate 10 to 0 using for loop, do the same using while and do while loop
+// //for loop
+// for (let i = 10; i >= 0; i--){
+//     console.log(i)
+// }
+
+// //while loop
+// let j = 10;
+// while(j >= 0){
+//     console.log(j)
+//     j--
+// }
+
+// //do while
+// let num = 10;
+// do{
+//     console.log(num)
+//     num--
+// } while(num >= 0);
+
+//3. Iterate 0 to n using for loop
+let n = 20;
+for(let i = 0; i <= n; i++){
+    console.log(`${i} of ${n}');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
