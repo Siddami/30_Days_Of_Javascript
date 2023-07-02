2ccc53f2fe5000b0a0b3d1f58d5a34a1af45242b
Siddami <dharmmy621@gmail.com>
2023-07-02 15:37:48 +0100

    project

--- days of js/day_21_js/level3.js
+++ days of js/day_21_js/level3.js
@@ -86,6 +86,7 @@ allLiElements.forEach((list, i) =>{
   list.style.margin = '10px auto'
   list.style.padding = '10px'
   list.style.textAlign = 'left'
+  list.style.width = '50%'
   if(list.textContent.includes('Done')){
     list.style.backgroundColor = 'green'
   }else if(list.textContent.includes('Ongoing')){
