d84f3fbb3be6d6451afc6814327a69cbe6ab4f43
Siddami <dharmmy621@gmail.com>
2023-07-07 16:00:58 +0100

    day 23

---
+++ days of js/day23_js/level1.js
@@ -0,0 +1,5 @@
+//get the elements in the intro header and style
+const header = document.querySelector('header')
+const h1 = document.querySelector('h1')
+const h5 = document.querySelector('h5')
+const p = document.querySelector('p')
