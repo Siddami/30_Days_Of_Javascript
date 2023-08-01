66d624e9c9e47cafbaf2060dd5ec8c4240248b72
Siddami <dharmmy621@gmail.com>
2023-08-01 16:54:36 +0100

    leaderboard

--- days of js/day28_js/index.js
+++ days of js/day28_js/index.js
@@ -11,6 +11,19 @@ const warning = document.querySelector('.warning')
 //get all country names from country data and save into a variable
 
 
+//current time
+const now = new Date()
+let month = now.getMonth()
+let day = now.getDate()
+day < 10 ? day = '0' + day: day;
+let year = now.getFullYear()
+let hour = now.getHours()
+hour < 10 ? hour = '0' + hour : hour;
+let minute = now.getMinutes()
+minute < 10 ? minute = '0' + minute: minute;
+const currentTime = `${month.toLocaleString()} ${day} ${year} ${hour}:${minute}`
+console.log(currentTime)
+
 //add event listener to button and create function to add new element when all fields are filled;
 subButton.addEventListener('click', () => {
     const fValue = firstName.value.trim()
@@ -20,4 +33,13 @@ subButton.addEventListener('click', () => {
     if(fValue === '' || lValue === '' || cValue === '' || sValue === ''){
         warning.textContent = 'All Fields Required'
     }
+
+   //create playborad container and add necessary elements based on input value
+  const playBoard = document.createElement('div')
+  playBoard.className = 'playerBoard'
+  const player = document.createElement('span')
+  player.className = 'player'
+  const name = document.createElement('p')
+  playBoard.textContent = `${firstName.toUpperCase()} ${lastName.toUpperCase()}`
+
 })
