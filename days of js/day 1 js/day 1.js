1a584d57f926bfb34a38c436895209c7f1f646d1
Siddami <dharmmy621@gmail.com>
2024-05-02 17:39:58 +0100

    corrected syntax

--- days of js/day 1 js/day 1.js
+++ days of js/day 1 js/day 1.js
@@ -1,5 +1,7 @@
 //1. comments can make code readable
+
 //2.  Welcome to 30DaysOfJavaScript
+
 /*3. comments can make 
 code readable, easy to reuse 
 and informative*/
@@ -11,12 +13,14 @@ let cage;
 let placeHolder;
 
 //7. Declare four variables with assigned values
+
 let var1 = "string";
 let var2 = 1;
 let var3 = false;
 let var4 = null;
 
 //8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
 let firstName = "Yetunde";
 let lastName = "Abdulkareem";
 let maritalStatus = "Single";
@@ -24,9 +28,11 @@ let country = "Nigeria";
 let age = 24;
 
 //.9 Declare variables to store your first name, last name, marital status, country and age in a single line
+
 let first_name = "Yetty" ,last_name = "Kareem", marital_status = "married", Country = "America", age_ = 24;
 
 //10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
 let myAge = 24;
 let yourAge = 30;
 
--- days of js/day 1 js/variable.js
+++ days of js/day 1 js/variable.js
@@ -1,4 +1,5 @@
 //.4 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
 let theString = "Hello";
 let booleanType = true;
 let notDefined;
